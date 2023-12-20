bd1720d71dd9d925f8d9867d3702f4258639d3af
RT44n <147970845+RT44n@users.noreply.github.com>
2023-12-20 18:11:21 +0530

    final version

--- index.js
+++ index.js
@@ -1,64 +1,84 @@
+const buttons = document.querySelectorAll("button");
+const pChoice = document.querySelector("#pChoice");
+const cChoice = document.querySelector("#cChoice");
+const pScore = document.querySelector("#playerScore");
+const cScore = document.querySelector("#computerScore");
+const winner = document.querySelector("#winner");
 
-
-function game()
-    {
-
-    const playerselection = prompt("Whats your choice?").toLowerCase(); 
-    const computerSelection = getComputerChoice();
-    let playerScore = 0;
-    let computerScore = 0;
-    console.log(computerSelection);
-    alert(playRound(playerselection, computerSelection));
-    alert("The player score is " + playerScore);
-    alert("The computer score is " + computerScore); 
-
-    function playRound(playerselection, computerSelection){
-        
-        if (playerselection == computerSelection){
-
-            return "Its a tie!";
-        }
-        if (playerselection == "rock" && computerSelection == "paper"){
-            computerScore++;
-            return "You have lost!";
-        }
-        if (playerselection == "rock" && computerSelection == "scissors"){
-            playerScore++;
-            return "You have won!";
-        }
-        if (playerselection == "paper" && computerSelection == "rock"){
-            playerScore++;
-            return "You have won!";
-        }   
-        if (playerselection == "paper" && computerSelection == "scissors"){
-            computerScore++;
-            return "You have lost!";
-        }
-        if (playerselection == "scissors" && computerSelection == "paper"){
-            playerScore++;
-            return "You have won!";
-        }
-        if (playerselection == "scissors" && computerSelection == "rock"){
-            computerScore++;
-            return "You have lost!";
-        } 
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playerselection = button.id;
+    computerSelection = getComputerChoice();
+    game();
+    pChoice.textContent = "You chose " + button.id;
+    cChoice.textContent = "The computer chose " + computerSelection;
+    pScore.textContent = "Your score is " + playerScore;
+    cScore.textContent = "The Computer score is " + computerScore;
+    if (playerScore == 5) {
+      winner.textContent = "You have won!";
+      computerScore = 0;
+      playerScore = 0;
+      alert("You have won!");
+    } else if (computerScore == 5) {
+      winner.textContent = "The computer has won!";
+      computerScore = 0;
+      playerScore = 0;
+      alert("You have lost!");
     }
+  });
+});
 
+let playerScore = 0;
+let computerScore = 0;
+let computerSelection = getComputerChoice();
 
-    function getComputerChoice()
-        {
-            let computerChoice = Math.floor(Math.random() * 3);
-        
-                if (computerChoice == 0){
-                    return "rock";
-                }
-                if  (computerChoice == 1 ){
-                    return "paper";
-                }
-                if (computerChoice == 2){
-                    return "scissors";
-                }
+function game() {
+  computerSelection;
+  playRound();
+}
 
-        }
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
 
-    }
+  if (computerChoice === 0) {
+    return "rock";
+  }
+  if (computerChoice === 1) {
+    return "paper";
+  }
+  if (computerChoice === 2) {
+    return "scissors";
+  }
+}
+const playRound = () => {
+  playerselection;
+  computerSelection;
+
+  if (playerselection == computerSelection) {
+    return "Its a tie!";
+  }
+  if (playerselection == "rock" && computerSelection == "paper") {
+    computerScore++;
+    return "You have lost!";
+  }
+  if (playerselection == "rock" && computerSelection == "scissors") {
+    playerScore++;
+    return "You have won!";
+  }
+  if (playerselection == "paper" && computerSelection == "rock") {
+    playerScore++;
+    return "You have won!";
+  }
+  if (playerselection == "paper" && computerSelection == "scissors") {
+    computerScore++;
+    return "You have lost!";
+  }
+  if (playerselection == "scissors" && computerSelection == "paper") {
+    playerScore++;
+    return "You have won!";
+  }
+  if (playerselection == "scissors" && computerSelection == "rock") {
+    computerScore++;
+    return "You have lost!";
+  }
+};
